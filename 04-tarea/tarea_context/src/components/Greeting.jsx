46ba59c31e96a586e4c524d8dbe02bae955fabc2
Valeria Riveros <92410347+ValRiv@users.noreply.github.com>
2022-11-09 17:59:16 -0300

    Delete 04-tarea/tarea_context/src directory

--- 04-tarea/tarea_context/src/components/Greeting.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { CurrentUserContext } from "./MyProviders";
-
-export default function Greeting() {
-  const { currentUser } = useContext(CurrentUserContext);
-  return <p>Ud. se ha logeado como: {currentUser.name}.</p>;
-}
